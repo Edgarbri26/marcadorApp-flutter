182d0b73aad70ca9ebe21c3d7546197d3db52d2f
SamuelRosales <samuelrosales.dias@gmail.com>
2025-09-07 21:28:18 -0400

    fix: update API endpoint for fetching players from localhost to production server

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -5,12 +5,11 @@ import 'package:marcador/models/jugadores.dart';
 
 class ApiService {
   Future<List<Jugador>> fetchJugadores() async {
-  final response = await http.get(Uri.parse('http://localhost:3000/api/player'));
+  final response = await http.get(Uri.parse('https://lpp-backend.onrender.com/api/player'));
 
   if (response.statusCode == 200) {
     final Map<String, dynamic> jsonBody = json.decode(response.body);
     final List<dynamic> jugadoresJson = jsonBody['data'];
-    print(jugadoresJson);
     return jugadoresJson.map((json) => Jugador.fromJson(json)).toList();
   } else {
     throw Exception('Error al cargar jugadores');
