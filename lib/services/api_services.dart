37519bd372220bf41953a14fb3d9046a3a8fb237
SamuelRosales <samuelrosales.dias@gmail.com>
2025-09-23 17:04:41 -0400

    fix: update pubspec.lock to adjust dependency versions and SDK constraints

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -7,8 +7,8 @@ import 'package:marcador/models/setResult.dart';
 /*import '../utils/constants.dart';*/
 
 class ApiService {
-  // String baseUrl = 'https://lpp-backend.onrender.com/api';
-  // String localUrl = 'http://localhost:3000/api';
+  //String baseUrl = 'https://lpp-backend.onrender.com/api';
+  //String localUrl = 'http://localhost:3000/api';
   String localUrl = 'https://lpp-backend.onrender.com/api';
 
   Future<List<Jugador>> fetchJugadores() async {
