5da4312e28d3a5480f23e3867763eff11f1dc06d
Edgarbriceno26@outlook.es <Edgarbriceno26@outlook.es>
2025-10-12 16:35:46 -0400

    compilando web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "083f6034fcc41bdd0c500481d7873817
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "363dad5a1cf9a2ce3b8f5757d736d815",
+"flutter_bootstrap.js": "93bb446ffcd47d66914d1a7121561e48",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
